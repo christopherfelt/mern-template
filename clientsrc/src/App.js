15563a74fa7a1121c688309b6525a3b4a59d4271
christopherfelt <chris.h.felt@gmail.com>
2021-05-18 08:09:11 -0600

    add animations to default pages

--- clientsrc/src/App.js
+++ clientsrc/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 import { Account } from "./context/Auth/Accounts";
 
@@ -15,17 +16,21 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/dist/lux/bootstrap.min.css";
 
 function App() {
+  const location = useLocation();
+
   return (
     <Account>
-      <Switch>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/about" exact component={AboutPage} />
-        <Route path="/contact" exact component={ContactPage} />
-        <Route path="/privacy" exact component={PrivacyPolicy} />
-        <Route path="/termsofservice" exact component={TermsOfService} />
-        <Route path="/auth" exact component={UserAuthorization} />
-        <Route path="/profile" exact component={ProfilePage} />
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.key}>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/about" exact component={AboutPage} />
+          <Route path="/contact" exact component={ContactPage} />
+          <Route path="/privacy" exact component={PrivacyPolicy} />
+          <Route path="/termsofservice" exact component={TermsOfService} />
+          <Route path="/auth" exact component={UserAuthorization} />
+          <Route path="/profile" exact component={ProfilePage} />
+        </Switch>
+      </AnimatePresence>
     </Account>
   );
 }
--- clientsrc/src/components/pages/default/Base.js
+++ clientsrc/src/components/pages/default/Base.js
@@ -1,13 +1,41 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import NavBar from "../../components/default/NavBar";
 import Footer from "../../components/default/Footer";
+
+const baseVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.25,
+      duration: 0.25,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.25,
+    },
+  },
+};
 
 const Base = (props) => {
   return (
     <div className="">
       <NavBar />
-      <div className="vh-100">{props.children}</div>
+      <motion.div
+        className="vh-100"
+        variants={baseVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+      >
+        {props.children}
+      </motion.div>
       <Footer />
     </div>
   );
